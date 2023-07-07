a0a61f0b46035cc15eff204a1d506aa5d30259b8
zeel9216 <137280950+zeel9216@users.noreply.github.com>
2023-07-07 22:22:16 +0530

    Add files via upload

---
+++ Day8/demo.js
@@ -0,0 +1,12 @@
+function green(){
+    let a=alert("fill body with background color green");
+    document.body.style.backgroundColor="green";
+}
+function red(){
+    let b=confirm("are you sure to fill body with background color red?");
+    document.getElementById("b2").style.backgroundColor="red";
+}
+function blue(){
+    prompt("which colour you have to fill body with background color?");
+    document.getElementById("b3").style.backgroundColor="blue";
+}
